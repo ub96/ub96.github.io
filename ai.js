ccaf63db87395d2c3a47bb6f08beb062c8fa2d27
ub96 <113331090+ub96@users.noreply.github.com>
2023-05-28 16:45:51 -0700

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,49 +1,29 @@
-// Function to prompt the user with a text box and get their input
-function promptInput(message) {
-    return prompt(message);
-}
+const apiKey = "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";
+const apiUrl = "https://api.openai.com/v1/engines/davinci-codex/completions";
 
-// Function to display an alert message
-function showAlert(message) {
-    alert(message);
-}
-
-// Function to start the conversation with ChatGPT
-function startChat() {
-    // Prompt the user for their message
-    const userInput = promptInput("Enter your message:");
-
-    // Send the user's message to the server
-    const url = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-    const data = {
-        'prompt': userInput,
-        'max_tokens': 50,
-        'temperature': 0.7
-    };
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer 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'
-        },
-        body: JSON.stringify(data)
+async function sendMessage(message) {
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      prompt: message,
+      max_tokens: 50  // Adjust the number of tokens as per your needs
     })
-    .then(response => response.json())
-    .then(response => {
-        // Retrieve the generated chatbot response
-        const chatbotResponse = response.choices[0].text.trim();
+  });
 
-        // Display the chatbot response in an alert
-        showAlert("ChatGPT: " + chatbotResponse);
-
-        // Start the conversation again
-        startChat();
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        showAlert('An error occurred. Please try again.');
-    });
+  const data = await response.json();
+  const reply = data.choices[0].text.trim();
+  return reply;
 }
 
-// Start the chat
-startChat();
+// Example usage
+async function chat() {
+  const userMessage = "Hello, how are you?";
+  const botReply = await sendMessage(userMessage);
+  console.log("Bot:", botReply);
+}
+
+chat();
