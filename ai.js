6b522baa6633c1a717deac1310b469d1d66c4929
ub96 <113331090+ub96@users.noreply.github.com>
2023-05-31 11:04:14 -0700

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,22 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
+    var chatziBuddyIcon = document.getElementById("chatzi-buddy-icon");
+    var chatziBuddy = document.getElementById("chatzi-buddy");
     var chatWindow = document.getElementById("chat-window");
     var messageBox = document.getElementById("message-box");
+
+    chatziBuddyIcon.addEventListener("click", function() {
+        chatziBuddy.style.display = "block";
+    });
 
     messageBox.addEventListener("keydown", function(event) {
         if (event.key === "Enter" && messageBox.value.trim() !== "") {
             var message = messageBox.value.trim();
 
-            // Clear the message box
-            messageBox.value = "";
-
-            // Create a new chat bubble for the user message
             createChatBubble("user", message);
 
-            // Generate a response for the user message
             var response = generateResponse(message);
 
-            // Create a new chat bubble for the response
             createChatBubble("chatzi", response);
+
+            messageBox.value = "";
         }
     });
 
